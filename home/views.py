988b327e6feed41c5bb5c65620533bc8f11c4f29
Naman <namansharma232009@gmail.com>
2021-06-22 14:17:01 +0530

    video 77

---
+++ home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,'home/home.html')
+
+def contact(request):
+    return render(request,'home/contact.html')
+
+def about(request):
+    return render(request,'home/about.html')
